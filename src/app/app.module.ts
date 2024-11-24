721beef6e0fcbc4497296a70fe60e8a7ef213279
Abdouul <abdelouahed.aitrahou@outlook.com>
2024-11-24 22:29:31 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"arrosage-43e17","appId":"1:743359944318:web:eb779ea61f88c520a36a2f","databaseURL":"https://arrosage-43e17-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"arrosage-43e17.firebasestorage.app","apiKey":"AIzaSyD0-CtWh7hjeeJSXb2ychI76jp32l4OqeI","authDomain":"arrosage-43e17.firebaseapp.com","messagingSenderId":"743359944318"})),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
